a9b3f0eb6ceac56a47e503797055fb0e3e3b3c25
Michal Bakshi <michalb5980@gmail.com>
2026-01-09 01:09:08 +0200

    Fix import statement for fs module #16

--- src/services/fileSystemService.ts
+++ src/services/fileSystemService.ts
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import fs from 'fs/promises';
 import { getYearMonthPath } from '@/services/urlRecordsService';
 
 export interface FileSystemError extends Error {
